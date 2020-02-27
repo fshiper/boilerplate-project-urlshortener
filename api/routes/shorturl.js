2dee9059142dbb7a6dd80863b3afd2efb96f130c
Glitch (fshiper-boilerplate-project-urlshortener) <none>
2020-02-27 14:47:48 +0000

    final commit

--- api/routes/shorturl.js
+++ api/routes/shorturl.js
@@ -1,10 +1,13 @@
 const ShortUrl = require("../../models/shorturl");
+const utils = require("../../utils/utils");
 const mongoose = require("mongoose");
+const dns = require("dns");
+const sha1 = require("sha1");
 
 module.exports = function(router) {
   router.get("/shorturl", (req, res) => {
     ShortUrl.find()
-      .select('-_id -__v')
+      .select("-_id -__v")
       .exec()
       .then(docs => res.status(200).json(docs))
       .catch(err =>
@@ -17,24 +20,54 @@ module.exports = function(router) {
 
   router.get("/shorturl/:url", (req, res) => {
     let shortUrl = req.params.url;
-    ShortUrl.findOne({short_url: shortUrl})
-      .select('original_url -_id')
+    ShortUrl.findOne({ short_url: shortUrl })
+      .select("original_url -_id")
       .exec()
       .then(doc => res.redirect(doc.original_url))
       .catch(err =>
         res.status(500).json({
-          message: "Error finding urls",
+          message: "Error finding given url",
           error: err
         })
       );
   });
 
   router.post("/shorturl/new", (req, res) => {
-    console.log(req.method + " " + req.path + " - " + JSON.stringify(req.body))
-    let shortUrl = new ShortUrl({ original_url: req.body.url, short_url: "test" });
-    shortUrl.save((err, data) => {
-      if (err) return res.status(400).json(err);
-      res.status(200).json(data);
-    });
+    let newShortUrl = {};
+    let inUrl = req.body.url;
+    utils()
+      .testUrl(inUrl)
+      .then(() => {
+        newShortUrl = new ShortUrl({
+          original_url: inUrl,
+          short_url: sha1(inUrl)
+        });
+
+        ShortUrl.findOne({ original_url: inUrl }, (err, data) => {
+          if (err) return res.status(400).json(err);
+          if (!data) {
+            console.log("saving new url..")
+            newShortUrl.save((err, data) => {
+              if (err) return res.status(400).json(err);
+              let response = {
+                original_url: data.original_url,
+                short_url: sha1(data.short_url)
+              };
+              res.status(200).json(response);
+            });
+          } else {
+            console.log("retrieving existing url..")
+            let response = {
+              original_url: data.original_url,
+              short_url: sha1(data.short_url)
+            };
+            res.status(200).json(response);
+          }
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
   });
 };
